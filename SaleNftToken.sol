a684f3ca5b0ecf0cb40417d404edfab4a9c3e76d
hokyunJung <everlips@naver.com>
2022-03-15 18:14:46 +0900

    Update SaleNftToken.sol

--- SaleNftToken.sol
+++ SaleNftToken.sol
@@ -11,9 +11,29 @@ contract SaleNftToken {
         xcube = Xcube(_xcubeTokenAddress);
     }
 
+    event purchase(address seller, address buyer, uint256 newTokenId, uint256 salePrice, uint256 buyPrice);
+
     mapping(uint256 => uint256) public nftTokenPrices;
 
     uint256[] public onSaleNftTokenArray;
+
+    //NFT 판매 취소
+    function setCancelForSale(uint256 _nftTokenId) public {
+        address nftTokenOwner = xcube.ownerOf(_nftTokenId);
+
+        require(nftTokenOwner == msg.sender, "You are not NFT token owner.");
+        require(nftTokenPrices[_nftTokenId] != 0, "This NFT token is not on sale.");
+        require(xcube.isApprovedForAll(nftTokenOwner, address(this)), "NFT token owner did not approve SaleNftToken.");
+
+        nftTokenPrices[_nftTokenId] = 0;
+
+        for(uint256 i = 0; i < onSaleNftTokenArray.length; i++) {
+            if(nftTokenPrices[onSaleNftTokenArray[i]] == 0) {
+                onSaleNftTokenArray[i] = onSaleNftTokenArray[onSaleNftTokenArray.length - 1];
+                onSaleNftTokenArray.pop();
+            }
+        }
+    }
 
     //NFT 를 팔기 위해 사용
     function setForSaleNftToken(uint256 _nftTokenId, uint256 _price) public {
@@ -22,7 +42,7 @@ contract SaleNftToken {
         require(nftTokenOwner == msg.sender, "You are not NFT token owner.");
         require(_price > 0, "Price is zero or hight.");
         require(nftTokenPrices[_nftTokenId] == 0, "This NFT token is already on sale.");
-        require(xcube.isApprovedForAll(nftTokenOwner, address(this)), "NFT token owner did not approve token.");
+        require(xcube.isApprovedForAll(nftTokenOwner, address(this)), "NFT token owner did not approve SaleNftToken.");
 
         nftTokenPrices[_nftTokenId] = _price;
 
@@ -31,12 +51,15 @@ contract SaleNftToken {
 
     //NFT 를 사기 위해 사용
     function purchaseNftToken(uint256 _nftTokenId) public payable {
-        uint256 price = nftTokenPrices[_nftTokenId];
+        uint256 salePrice = nftTokenPrices[_nftTokenId];
         address nftTokenOnwer = xcube.ownerOf(_nftTokenId);
 
-        require(price > 0, "NFT token is not on sale.");
-        require(price <= msg.value, "You sent lower than price.");
+        require(salePrice > 0, "NFT token is not on sale.");
+        require(salePrice <= msg.value, "You sent lower ether than sale price.");
         require(nftTokenOnwer != msg.sender, "You are this NFT token owner.");
+
+        //purchase(address seller, address buyer, uint256 newTokenId, uint256 salePrice, uint256 buyPrice);
+        emit purchase(nftTokenOnwer, msg.sender, _nftTokenId, salePrice, msg.value);
 
         payable(nftTokenOnwer).transfer(msg.value);
         xcube.safeTransferFrom(nftTokenOnwer, msg.sender, _nftTokenId);
